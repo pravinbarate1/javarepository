6d2ffc923590b4f6c92403f118163e9121ad31dd
Pravin Barate <pbarate@pbarate.pnq.csb>
2018-05-11 15:17:47 +0530

    Servlet Filter

---
+++ ServletFilterTest/src/com/rh/servlet/LoginServlet.java
@@ -0,0 +1,57 @@
+package com.rh.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private final String userID = "admin";
+	private final String password = "password";
+
+    public LoginServlet() {
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String user = request.getParameter("user");
+		String pwd = request.getParameter("pwd");
+		
+		if(userID.equals(user) && password.equals(pwd))
+		{
+			HttpSession session = request.getSession();
+			
+			session.setAttribute("user", "Pravin");
+			session.setMaxInactiveInterval(30*60);
+			
+			Cookie userName = new Cookie("user", user);
+			userName.setMaxAge(30*60);
+			
+			response.addCookie(userName);
+			response.sendRedirect("LoginSuccess.jsp");
+		}else{
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/Login.html");
+			PrintWriter out = response.getWriter();
+			
+			out.println("<font color=red>Either user name or password is wrong.</font>");
+			rd.include(request, response);
+			
+		}
+		
+		doGet(request, response);
+	}
+
+}
---
+++ ServletFilterTest/src/com/rh/servlet/LogoutServlet.java
@@ -0,0 +1,47 @@
+package com.rh.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LogoutServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+		
+		response.setContentType("text/html");
+		Cookie[] cookies = request.getCookies();
+		
+		if(cookies != null) {
+			for(Cookie cookie : cookies) {
+				if(cookie.getName().equals("JSESSIONID")) {
+					System.out.println("JSESSIONID="+cookie.getValue());
+					break;
+					
+				}
+			}
+		}
+		HttpSession session = request.getSession(false);
+		System.out.println("User="+session.getAttribute("user"));
+		if(session != null) {
+			session.invalidate();
+		}
+		response.sendRedirect("Login.html");
+	}
+
+}
