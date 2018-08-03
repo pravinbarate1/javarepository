f300bd6addc0c2c61f6e583ed8d3f6440dbb6464
Pravin Barate <pbarate@pbarate.pnq.csb>
2018-08-03 13:02:27 +0530

    REST service using RestEasy with jboss

---
+++ RESTfulDemoApplication/src/main/java/com/pravin/ApplicationConfig.java
@@ -0,0 +1,18 @@
+package com.pravin;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+import com.pravin.service.UserService;
+
+@ApplicationPath("/")
+public class ApplicationConfig extends Application {
+    @SuppressWarnings("unchecked")
+	public Set<Class<?>> getClasses() {
+        return new HashSet<Class<?>>(Arrays.asList(UserService.class));
+    }
+}
