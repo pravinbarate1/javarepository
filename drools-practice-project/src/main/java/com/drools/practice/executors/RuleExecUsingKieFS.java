ecbc3f8e4835814960abca34a20e163fbae8560b
Pravin Barate <pravinbarate1@gmail.com>
2019-07-29 01:33:18 +0530

    some KFS and resources ex

--- drools-practice-project/src/main/java/com/drools/practice/executors/RuleExecUsingKieFS.java
+++ drools-practice-project/src/main/java/com/drools/practice/executors/RuleExecUsingKieFS.java
@@ -4,14 +4,19 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
+import org.junit.Assert;
 import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
 import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.Message;
 import org.kie.api.builder.model.KieBaseModel;
 import org.kie.api.builder.model.KieModuleModel;
 import org.kie.api.builder.model.KieSessionModel;
 import org.kie.api.builder.model.KieSessionModel.KieSessionType;
 import org.kie.api.conf.EqualityBehaviorOption;
 import org.kie.api.conf.EventProcessingOption;
+import org.kie.api.io.ResourceType;
+import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.conf.ClockTypeOption;
 
 public class RuleExecUsingKieFS {
@@ -34,7 +39,14 @@ public class RuleExecUsingKieFS {
 		
 		KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
 		
-		kieFileSystem.writeKModuleXML(kieModuleModel.toXML());
+		
+		kieServices.newKieBuilder(kieFileSystem).buildAll();
+		
+		KieContainer kieContainer = kieServices.newKieContainer(kieServices.getRepository().getDefaultReleaseId());
+		
+		KieBuilder kieBuilder = kieServices.newKieBuilder(kieFileSystem).buildAll();
+		
+		Assert.assertEquals(0, kieBuilder.getResults().getMessages(Message.Level.ERROR).size());
 		
 		
 		  InputStream inputStream = new
@@ -44,7 +56,12 @@ public class RuleExecUsingKieFS {
 						
 		  kieFileSystem.write("src/main/resources/com/drools/practice/dtables/Sample1.xls",
 							  kieServices.getResources().newInputStreamResource(inputStream));
+		  
+		  InputStream drlInStream = new
+				  FileInputStream("/home/pbarate/Desktop/GreetingRule.drl");
+		  kieFileSystem.write("src/main/resources/com/drools/practice/rules/myDRL.txt", kieServices.getResources().newInputStreamResource(drlInStream).setResourceType(ResourceType.DRL));
 							 
+		  kieFileSystem.writeKModuleXML(kieModuleModel.toXML());
 		 
 		  System.out.println("Exiting from the RuleExecUsingKieFS");
 		 
